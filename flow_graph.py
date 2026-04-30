24710f30ede221f569b28a34794cb368a3e8f1b7
101369 <101369@MACTECH.LOC>
2026-04-30 14:52:36 +0530

    simple test

--- flow_graph.py
+++ flow_graph.py
@@ -1,35 +1,17 @@
+from langgraph.graph import START, END, StateGraph
+from langchain_core.messages import SystemMessage
+from state_node import StateNode
 from functools import partial
 
-from langgraph.graph import START, StateGraph
-from langchain_core.messages import HumanMessage, SystemMessage
-from state_node import StateNode
+async def chat(state: StateNode, chatllm):
+    messages = state["messages"]
+    system_msg = SystemMessage(content="You are a helpful assistant.")
+    response = await chatllm.ainvoke([system_msg, *messages])
+    return {"messages": [response]}
 
-async def Orchesterator(state: StateNode,chatllm):
-    """Orchestrator for the Telegram Agentic Bot."""
-    messages = state["messages"][-1]
-    system_msg = SystemMessage(content="You are a helpful assistant that provides concise user intent information.from the following intent : General Chat,insurance supporty")
-    intent = await chatllm.ainvoke([system_msg, messages])
-    return {"messages":[intent]}
-
-async def Analyze_Photo(state: StateNode,chatllm):
-    """Analyze photo content."""
-    messages = state.get("messages")[-1]
-    system_msg = "You are an image analysis assistant. get all the content of the image in detail. for future Analysis"
-    analysis = await chatllm.ainvoke({
-        "messages": [system_msg, *messages]
-    })
-    return analysis["messages"][-1].content
-
-
-
-def create_flow_graph(chatllm)->StateGraph:
-    """Create a flow graph for the Telegram Agentic Bot."""
-    graph=StateGraph(StateNode)
-    graph.add_node("Router",partial(Orchesterator, chatllm=chatllm))
-    graph.add_node("Analyze",partial(Analyze_Photo, chatllm=chatllm))
-
-    graph.add_edge(START, "Router")
-    graph.add_edge("Router","Analyze")
-
-
+def create_flow_graph(chatllm):
+    graph = StateGraph(StateNode)
+    graph.add_node("chat", partial(chat, chatllm=chatllm))
+    graph.add_edge(START, "chat")
+    graph.add_edge("chat", END)
     return graph.compile()
