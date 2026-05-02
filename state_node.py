6f52721629ddca0e66385b5f697d2fcaf996bab5
101369 <101369@MACTECH.LOC>
2026-05-02 14:25:05 +0530

    updated the qdrant on server side

--- state_node.py
+++ state_node.py
@@ -1,11 +1,11 @@
 from typing import Annotated, Any, Optional, TypedDict
 from groq import BaseModel
-from langchain_google_genai import ChatGoogleGenerativeAI
+
 from langgraph.graph.message import add_messages
 from typing import Literal
 
 class AppState:
-    llm : ChatGoogleGenerativeAI = None
+    llm = None
     agent = None
     retriever = None
     qdrant_client = None
